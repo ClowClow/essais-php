53dc2d5629542f55dfacd696959b5ee632a6d800
ClowClow <c.servieres@simplon-charleville.fr>
2019-04-23 15:35:46 +0200

    questions 2, 3 et 4

--- index.php
+++ index.php
@@ -12,13 +12,35 @@
 <body>
   <h1>Page d'essai de Php</h1>
 
-Question 1 : Afficher le plus petit entre A et B; 
+Question 1 : Afficher le plus petit entre A et B;<br/>
   <?php
   $A = 20;
   $B = 12;
 echo min($B, $A);
   ?>
-
+<br/>
+Question 2 : Afficher le plus petit entre A, B et C;<br/>
+<?php
+$A = 30;
+$B = 0;
+$C = -3;
+echo min($A, $B, $C);
+?>
+<br/>
+Question 3 : Afficher un nombre aléatoire entre 1 et 100;<br/>
+<?php
+$min = 1;
+$max = 100;
+echo rand($min, $max);
+?>
+<br/>
+Question 4 : Afficher un nombre aléatoire entre 5 et 100;<br/>
+<?php
+$min = 5;
+$max = 100;
+echo rand($min, $max);
+?>
+<br/>
 </body>
 
 </html>
