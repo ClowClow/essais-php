3f6a81cf37f42a7199babc14b4419c538c51f139
ClowClow <c.servieres@simplon-charleville.fr>
2019-04-23 15:18:58 +0200

    question 1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Essais PHP</title>
+</head>
+
+<body>
+  <h1>Page d'essai de Php</h1>
+
+Question 1 : Afficher le plus petit entre A et B; 
+  <?php
+  $A = 20;
+  $B = 12;
+echo min($B, $A);
+  ?>
+
+</body>
+
+</html>
